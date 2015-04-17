9d3f72fdb113c3fd48927ff01efbb552b2042ebc
Johan Swensson <jswensso@redhat.com>
2015-04-17 08:02:49 +0000

    add example sysctl module

---
+++ modules/opuk-sysctl/manifests/init.pp
@@ -0,0 +1,49 @@
+# == Class: sysctl
+#
+# Full description of class sysctl here.
+#
+# === Parameters
+#
+# Document parameters here.
+#
+# [*sample_parameter*]
+#   Explanation of what this parameter affects and what it defaults to.
+#   e.g. "Specify one or more upstream ntp servers as an array."
+#
+# === Variables
+#
+# Here you should define a list of variables that this module would require.
+#
+# [*sample_variable*]
+#   Explanation of how this variable affects the funtion of this class and if
+#   it has a default. e.g. "The parameter enc_ntp_servers must be set by the
+#   External Node Classifier as a comma separated list of hostnames." (Note,
+#   global variables should be avoided in favor of class parameters as
+#   of Puppet 2.6.)
+#
+# === Examples
+#
+#  class { sysctl:
+#    servers => [ 'pool.ntp.org', 'ntp.local.company.com' ],
+#  }
+#
+# === Authors
+#
+# Author Name <author@domain.com>
+#
+# === Copyright
+#
+# Copyright 2015 Your name here, unless otherwise noted.
+#
+class sysctl (
+  $parameters = [],
+) {
+
+  file { '/etc/sysctl.d/99-custom-sysctl.conf':
+    mode => 0644,
+    owner => root,
+    group => root,
+    content => template("sysctl/99-custom-sysctl.conf.erb")
+  }
+
+}
